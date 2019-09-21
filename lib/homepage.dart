fb5eee47d528e866de6796cebd00a8ea5a766afa
riadshuvo <riadshuvo55@gmail.com>
2019-09-21 13:36:37 +0600

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+
+import 'package:firebase_phone_auth/main.dart';
+import 'package:flutter/material.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class DashboardPage extends StatefulWidget {
+  String uid;
+  DashboardPage({this.uid});
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+
+
+  getUid() {}
+
+  /*@override
+  void initState() {
+    this.widget.uid = '';
+    FirebaseAuth.instance.currentUser().then((val) {
+      setState(() {
+        this.widget.uid = val.uid;
+      });
+    }).catchError((e) {
+      print(e);
+    });
+    super.initState();
+  }
+*/
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Dashboard'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text('You are now logged in as ${widget.uid}'),
+                SizedBox(
+                  height: 15.0,
+                ),
+                new OutlineButton(
+                  borderSide: BorderSide(
+                      color: Colors.red, style: BorderStyle.solid, width: 3.0),
+                  child: Text('Logout'),
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut().then((action) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MyApp()),
+                      );
+                    }).catchError((e) {
+                      print(e);
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,167 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_phone_auth/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Firebase PhoneAuth'),
+        routes: <String, WidgetBuilder>{
+          '/homepage': (BuildContext context) => DashboardPage(),
+          '/landingpage': (BuildContext context) => MyHomePage()
+        });
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String phoneNo;
+  String uid;
+  String smsCode;
+  String verificationId;
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+            padding: EdgeInsets.all(25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(hintText: 'Enter Phone number'),
+                  onChanged: (value) {
+                    this.phoneNo = value;
+                  },
+                ),
+                SizedBox(height: 10.0),
+                RaisedButton(
+                    onPressed: verifyPhone,
+                    child: Text('Verify'),
+                    textColor: Colors.white,
+                    elevation: 7.0,
+                    color: Colors.blue)
+              ],
+            )),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future<void> verifyPhone() async {
+    final PhoneCodeAutoRetrievalTimeout autoRetrieve = (String verId) {
+      this.verificationId = verId;
+    }; // this will auto click the verification button
+
+    final PhoneCodeSent smsCodeSent = (String verId, [int forceCodeResend]) {
+      this.verificationId = verId;
+      smsCodeDialog(context).then((value) {
+        print("Signed In");
+      });
+    };
+
+    final PhoneVerificationCompleted verificationCompleted =
+        (AuthCredential user) {
+      print('verified');
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => DashboardPage()),
+      );
+    };
+
+    final PhoneVerificationFailed verificationFailed =
+        (AuthException expection) {
+      print(expection.message);
+    };
+
+    await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: this.phoneNo,
+        timeout: const Duration(seconds: 5),
+        verificationCompleted: verificationCompleted,
+        verificationFailed: verificationFailed,
+        codeSent: smsCodeSent,
+        codeAutoRetrievalTimeout: autoRetrieve);
+  }
+
+  Future<bool> smsCodeDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+            title: Text('Enter sms Code'),
+            content: TextField(
+              onChanged: (value) {
+                this.smsCode = value;
+              },
+            ),
+            contentPadding: EdgeInsets.all(10.0),
+            actions: <Widget>[
+              new FlatButton(
+                child: Text('Done'),
+                onPressed: () {
+                  FirebaseAuth.instance.currentUser().then((user) {
+                    if (user != null) {
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pushReplacementNamed('/homepage');
+                    } else {
+                      Navigator.of(context).pop();
+                      signIn(smsCode);
+// Find the Scaffold in the widget tree and use it to show a SnackBar.
+
+                    }
+                  });
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  signIn(String smsCode) async {
+    final AuthCredential credential = PhoneAuthProvider.getCredential(
+      verificationId: verificationId,
+      smsCode: smsCode,
+    );
+
+    FirebaseAuth.instance.currentUser().then((val) {
+      setState(() {
+        this.uid = val.uid;
+      });
+    }).catchError((e) {
+      print(e);
+    });
+
+    FirebaseAuth _auth = FirebaseAuth.instance;
+    final FirebaseUser user =
+        await _auth.signInWithCredential(credential).then((user) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => DashboardPage(uid: user.user.uid)),
+      );
+    }).catchError((e) {
+      print(e);
+    });
+  }
+}
